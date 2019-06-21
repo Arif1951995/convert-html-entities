cefea770d54bf2125874cb20fa1173410053a607
Arif1951995 <40889693+Arif1951995@users.noreply.github.com>
2019-06-21 16:11:25 +0500

    Add files via upload

---
+++ convert-html-intities.js
@@ -0,0 +1,35 @@
+function convertHTML(str) {
+  // &colon;&rpar;
+  let newStr = "";
+  for(let i = 0; i < str.length; i++) {
+      switch(str[i]) {
+        case '&':
+        newStr += '&​amp;';
+        break;
+        case '<':
+        newStr += '&​lt;';
+        break;
+        case '>':
+        newStr += '&​gt;';
+        break;
+         case '"':
+        newStr += '&​quot;';
+        break;
+          case "'":
+        newStr += '&​apos;';
+        break;
+        default:
+      newStr += str[i];
+      break;
+}
+}
+return newStr;  
+}
+console.log(convertHTML("Dolce & Gabbana")); 
+
+
+
+
+
+
+
